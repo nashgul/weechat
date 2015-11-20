59a599c41b83a47f8aaace756d76832855192766
nashgul <sinp13@gmail.com>
2015-11-20 20:19:06 +0100

    first commit

---
+++ audacious_script/audacious.py
@@ -0,0 +1,49 @@
+# audacious now playing for weechat
+# nashgul <m.alcocer1978@gmail.com>
+# version 0.1
+
+# white => "00", black => "01", darkblue => "02", darkgreen => "03", lightred => "04",
+# darkred => "05", magenta => "06", orange => "07", yellow => "08", lightgreen => "09",
+# cyan => "10", lightcyan => "11", lightblue => "12", lightmagenta => "13", gray => "14",
+# lightgray => "15"
+
+import weechat
+import subprocess
+
+weechat.register("audacious_np", "nashgul", "0.01", "GPL2", "now playing for audacious (usage: /audacious)", "", "")
+
+name = 'audacious'
+description = 'show now playing for audacious'
+hook = weechat.hook_command(name, description, '', '', '', 'now_playing', '')
+
+def get_info_array():
+    info_list = (['audtool current-song',
+        'audtool current-song-length',
+        'audtool current-song-output-length',
+        'audtool current-song-bitrate-kbps',
+        'audtool current-song-filename'])
+    results = []
+    for x in info_list:
+        temporal = subprocess.Popen(x, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        b = temporal.stdout.read().splitlines()
+        results.append(b[0])
+    return results
+
+def now_playing(datos, channel, args):
+    message = 'esta reproduciendo'
+    info_array = get_info_array()
+    song = info_array[0]
+    length = info_array[1]
+    output_length = info_array[2]
+    bitrate = info_array[3]
+    song_filename = info_array[4]
+    if length == '0:00':
+        string = "[ %s - %s kbps ]" %(output_length, bitrate)
+    else:
+        string = "[ %s - %s @ %s kbps ]" %(output_length, length, bitrate)
+    source = ''
+    if song_filename.lower().startswith('http'):
+        source = song_filename
+    output_string = "%s : %s %s %s" %(message, source, song, string)
+    weechat.command(channel, "/me %s" % (output_string))
+    return weechat.WEECHAT_RC_OK
