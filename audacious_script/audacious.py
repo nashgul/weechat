a24276c14641638f929d9b37f07cf1f36fbb0328
nashgul <sinp13@gmail.com>
2015-11-20 22:16:23 +0100

    first commit

--- audacious_script/audacious.py
+++ audacious_script/audacious.py
@@ -2,10 +2,7 @@
 # nashgul <m.alcocer1978@gmail.com>
 # version 0.1
 
-# white => "00", black => "01", darkblue => "02", darkgreen => "03", lightred => "04",
-# darkred => "05", magenta => "06", orange => "07", yellow => "08", lightgreen => "09",
-# cyan => "10", lightcyan => "11", lightblue => "12", lightmagenta => "13", gray => "14",
-# lightgray => "15"
+# white => "00", black => "01", darkblue => "02", darkgreen => "03", lightred => "04", darkred => "05", magenta => "06", orange => "07", yellow => "08", lightgreen => "09", cyan => "10", lightcyan => "11", lightblue => "12", lightmagenta => "13", gray => "14", lightgray => "15"
 
 import weechat
 import subprocess
@@ -30,20 +27,43 @@ def get_info_array():
     return results
 
 def now_playing(datos, channel, args):
-    message = 'esta reproduciendo'
+    colors = {
+            'white' : '00', 'black' : '01', 'darkblue' : '02', 'darkgreen' : '03',
+            'lightred' : '04', 'darkred' : '05', 'magenta' : '06', 'orange' : '07',
+            'yellow' : '08', 'lightgreen' : '09', 'cyan' : '10', 'lightcyan' : '11',
+            'lightblue' : '12', 'lightmagenta' : '13', 'gray' : '14', 'lightgray' : '15'
+            }
+    
     info_array = get_info_array()
-    song = info_array[0]
-    length = info_array[1]
-    output_length = info_array[2]
-    bitrate = info_array[3]
-    song_filename = info_array[4]
+
+    message_color = "%s" % colors['darkblue']
+    message = u'\x03' + message_color + 'esta reproduciendo' + u'\x0f'
+    song_color = "%s" % colors['lightred']
+    song = u'\x03' + song_color + info_array[0] + u'\x0f'
+    song_filename_color = "%s" % colors['lightred']
+    song_filename = u'\x03' + song_filename_color + info_array[4] + u'\x0f'
+    brackets_color = "%s" % colors['yellow']
+    bracket_1 = u'\x03' + brackets_color + '[' + u'\x0f' 
+    bracket_2 = u'\x03' + brackets_color + ']' + u'\x0f'
+    hyphen_color = "%s" % colors['yellow']
+    hyphen = u'\x03' + hyphen_color + '-' + u'\x0f'
+    at_color = "%s" % colors['yellow']
+    at_sym = u'\x03' + at_color + '@' + u'\x0f'
+    output_length_color = "%s" % colors['lightblue']
+    output_length = u'\x03' + output_length_color + info_array[2] + u'\x0f'
+    length_color = "%s" % colors['lightblue']
+    length = u'\x03' + length_color + info_array[1] + u'\x0f'
+    bitrate_color = "%s" % colors['lightmagenta']
+    bitrate = u'\x03' + bitrate_color + info_array[3] + ' kbps' + u'\x0f'
+
     if length == '0:00':
-        string = "[ %s - %s kbps ]" %(output_length, bitrate)
+        string = "%s %s %s %s %s" %(bracket_1, output_length, at_sym, bitrate, bracket_2)
     else:
-        string = "[ %s - %s @ %s kbps ]" %(output_length, length, bitrate)
+        string = "%s %s %s %s %s %s %s" %(bracket_1, output_length, hyphen, length, at_sym, bitrate, bracket_2)
     source = ''
     if song_filename.lower().startswith('http'):
-        source = song_filename
-    output_string = "%s : %s %s %s" %(message, source, song, string)
+        source = song_filename 
+    output_string = "%s: %s %s %s" %(message, source, song, string)
     weechat.command(channel, "/me %s" % (output_string))
     return weechat.WEECHAT_RC_OK
+
